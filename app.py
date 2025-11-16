6ef7ad14002f4686b17e7d722962f2dfba82911b
Shrijit Naukarkar <shrijitnaukarkar1729@gmail.com>
2025-11-17 00:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,39 @@ from PyPDF2 import PdfWriter
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-    with open("index.html", "r") as f:
+    if request.method=="POST":
+        num = request.form.get("num_files")
+        if num and num.isdigit() and int(num) > 0:
+            num = int(num)
+            with open("upload.html","r") as f:
+                html = f.read()
+            inputs_html = ""
+            for i in range(1,num+1):
+                inputs_html += (f"File {i} : <input type='file' name='pdf{i}'><br><br>\n")
+            html = html.replace("{{inputs}}",inputs_html)
+            return html
+        else:
+            return ("Enter a valid number of files.")
+    with open("index.html","r") as f:
         return f.read()
 
 @app.route("/merge", methods=["POST"])
-def merge_pdfs():
+def merge():
+    num = int(request.form.get("num_files"))
     merger = PdfWriter()
-    pdfs = []
-
-    uploaded = request.files.getlist("pdffiles")
-    n = len(uploaded)
-
-    for i in range(n):
-        pdfs.append(uploaded[i])
-
-    for pdf in pdfs:
-        merger.append(pdf)
-
-    output = "merged-pdf.pdf"
+    for i in range(1,num+1):
+        f = request.files.get(f"pdf{i}")
+        if f:
+            merger.append(f)
+    output = ("merged.pdf")
     merger.write(output)
     merger.close()
-
     return send_file(output, as_attachment=True)
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
+
+
 
